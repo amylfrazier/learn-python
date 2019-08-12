d83628351f1e60c071a844652ddb855229dedd90
Amy Frazier <amyf7475@gmail.com>
2019-08-11 20:11:49 -0500

    added printing functions to print name and age

--- hw1/main.py
+++ hw1/main.py
@@ -1,5 +1,14 @@
 def main():
     print("hello world")
+    print("What is your name?") #ask for their name
+    myName = input()
+    print("It is good to meet you, " + myName)
+    print("The length of your name is:")
+    print(len(myName))
+    print("What is your age?")
+    myAge = input()
+    print(type (myAge))
+    print("You will be " + str(int(myAge)+1) + " in a year.")
 
 if __name__ == "__main__":
     main()
