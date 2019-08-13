bb863f5018348b01e465d3042cbe341f37f7fc5c
Amy Frazier <amyf7475@gmail.com>
2019-08-13 09:43:22 -0500

    asked for names, age, returned name and works

--- hw1/main.py
+++ hw1/main.py
@@ -1,6 +1,10 @@
 def main():
     print("What is your first name?")
     myFirstName = input()
-
+    print("What is your last name?")
+    myLastName = input()
+    print("How old are you in months?")
+    myAgeMonths = input()
+    print("Your full name is " + myFirstName + " " + myLastName + ".")
 if __name__ == "__main__":
     main()
