d98151d24e3f8385464f98039033753f675037b6
Amy Frazier <amyf7475@gmail.com>
2019-08-14 16:32:01 -0500

    Whole thing works with blurb at end in years and months

--- hw1/main.py
+++ hw1/main.py
@@ -6,6 +6,6 @@ def main():
     print("How old are you in months?")
     myAgeMonths = input()
     print("Your full name is " + myFirstName + " " + myLastName + ".")
-    print( "You are " + str(float(myAgeMonths)/12) + " years old.")
+    print( "You are " + str(int(myAgeMonths)//12) + " years old and " + str(int(myAgeMonths)%12) + " months old.")
 if __name__ == "__main__":
     main()
