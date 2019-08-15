41d80521e1ff40631e9cdf94fca7c1915add3f2f
Amy Frazier <amyf7475@gmail.com>
2019-08-14 21:08:02 -0500

    removed spacing errors and spelling mistakes

--- hw1/main.py
+++ hw1/main.py
@@ -9,13 +9,13 @@ def main():
     myColor = input()
     print("Where were you born?")
     myBirthPlace = input()
-    print("How many people are in yours family?")
+    print("How many people are in your family?")
     myFamily = input()
     print("What is the brand of your favorite bubbly water?")
     myWater = input()
 
     print( "Your full name is " + myFirstName + " " + myLastName + ". " + "You are " + str(int(myAgeMonths)//12) + " years and " + str(int(myAgeMonths)%12) + " months old.")
-    print( "Your favorite color is " + myColor + " .")
+    print( "Your favorite color is " + myColor + ".")
     print( "You were born in " + myBirthPlace + " and you have " + myFamily + " people in your family.")
     print( "Oh and your favorite bubbly water is that tsp tsp tsp ahhhh " + myWater + "!")
 if __name__ == "__main__":
